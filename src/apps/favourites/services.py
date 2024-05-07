2fc61d83bceaec761f0f40e3acc877c77e3c2b9e
MaksimLogvinov <m.logvinov@wis.software>
2024-05-07 04:34:36 +0300

    feat: update services

--- src/apps/favourites/services.py
+++ src/apps/favourites/services.py
@@ -1,14 +1,15 @@
 import logging
-from os.path import exists
+from decimal import Decimal, ROUND_HALF_UP
 from typing import Optional, List, Tuple
 
 from fastapi import HTTPException
+from sqlalchemy.orm import selectinload
 from sqlalchemy.sql import Select, select, func
 
 from apps.commons.pagination.schemas import Pagination
 from apps.commons.services import ServiceBase
 from apps.favourites.schemas import FavouriteIn
-from db.models import Favourite
+from db.models import Favourite, Product, Review
 
 logger = logging.getLogger('favourites')
 
@@ -19,8 +20,10 @@ class FavouriteService(ServiceBase):
     async def create(self, *, data: FavouriteIn = None, data_extra: Optional[dict] = None) -> Model:
         data = (await self.validate_data(None, data)).dict(exclude_unset=True) if data else dict()
 
+        await self.check_product_or_404(data)
+
         if await self.check_exists(**data):
-            raise HTTPException(status_code=409, detail="Товар уже довлен в понравившиеся")
+            raise HTTPException(status_code=409, detail="The product already added in favourites")
         favourite = await self.manager.create(
             self.Model,
             data | {"id_user": int(self.id_user)}
@@ -29,8 +32,15 @@ class FavouriteService(ServiceBase):
         return favourite
 
     async def delete(self, id_instance: int):
-        return await self.manager.delete((await self.manager.execute(
-            self.select_visible().where(self.Model.id_product == id_instance))).scalars().first())
+        instance = (await self.manager.execute(
+            self.select_visible().where(
+                self.Model.id_product == id_instance,
+                self.Model.id_user == int(self.id_user))
+        )).scalars().first()
+        if not instance:
+            raise HTTPException(status_code=404, detail="The product not in favourites")
+
+        return await self.manager.delete(instance)
 
     async def list(
         self,
@@ -55,11 +65,52 @@ class FavouriteService(ServiceBase):
             pagination=pagination
         )
 
+    async def list_favourites(
+            self,
+            *,
+            filters: Optional[List] = None,
+            orderings: Optional[List] = None,
+            pagination: Pagination = None,
+            query: Optional[Select] = None,
+    ):
+        result = await self.list(filters=filters, orderings=orderings, pagination=pagination, query=query)
+        for product in result['items']:
+            await self.get_rating_and_reviews_count(product.product)
+            product.product.is_favourite = True
+        return result
+
     async def get_fragment(self, query: Select, limit: Optional[int], offset: int) -> Tuple[List, int]:
         query_count = select(func.count(1)).select_from(query)
         return (
             (await self.manager.execute(
-                query.limit(limit).offset(offset).filter(Favourite.id_user == int(self.id_user)))
-             ).scalars().all(),
+                query.options(
+                    selectinload(Favourite.product).selectinload(Product.photos)
+                )
+                .join(Favourite.product)
+                .filter(Favourite.id_user == int(self.id_user))
+                .offset(offset)
+            )).scalars().all(),
             (await self.manager.execute(query_count)).scalar()
         )
+
+    async def get_rating_and_reviews_count(self, instance: Model) -> Model:
+        reviews_count_subquery = (
+            select(func.count(Review.id))
+            .where(Review.id_product == instance.id)
+            .scalar_subquery()
+        )
+        average_rating_subquery = (
+            select(func.avg(Review.rating))
+            .where(Review.id_product == instance.id)
+            .scalar_subquery()
+        )
+
+        instance.reviews_count, instance.average_rating = (
+            await self.manager.execute(
+                select([reviews_count_subquery, average_rating_subquery])
+            )
+        ).first()
+
+        if instance.average_rating is not None:
+            instance.average_rating = Decimal(instance.average_rating).quantize(Decimal('0.0'), rounding=ROUND_HALF_UP)
+        return instance
--- src/apps/products/services.py
+++ src/apps/products/services.py
@@ -1,21 +1,31 @@
 import logging
+from decimal import ROUND_HALF_UP
 from typing import Optional, List, Tuple
 
-from sqlalchemy import func, select
-from sqlalchemy.orm import selectinload, selectin_polymorphic, joinedload
+from fastapi import HTTPException
+from pydantic.types import Decimal
+from sqlalchemy import func, select, exists
+from sqlalchemy.orm import selectinload, selectin_polymorphic
 from sqlalchemy.sql import Select
 
+from apps.commons.managers.base import ManagerBase
 from apps.commons.pagination.schemas import Pagination
 from apps.commons.services.base import ServiceBase, ServiceAuthenticate
 from apps.favourites.services import FavouriteService
 from apps.products.schemas import ProductIn
-from db.models import Product, Review, Tablet, Accessory, Television, Smartphone, Smartwatch, Laptop, User
+from db.models import Product, Review, Tablet, Accessory, Television, Smartphone, Smartwatch, Laptop, User, Order, \
+    OrderItem
+from settings import settings_app
 
 logger = logging.getLogger('products')
 
 
 class ProductService(ServiceBase):
     Model = Product
+
+    def __init__(self, manager: ManagerBase, id_user, *args, **kwargs) -> None:
+        super().__init__(manager, id_user, *args, **kwargs)
+        self._addons_base = [self.Model.is_deleted.is_(False)]
 
     async def create(self, *, data: ProductIn = None, data_extra: Optional[dict] = None) -> Model:
         product = await super().create(data=data, data_extra=data_extra)
@@ -39,16 +49,67 @@ class ProductService(ServiceBase):
                 select([reviews_count_subquery, average_rating_subquery])
             )
         ).first()
+
+        if instance.average_rating is not None:
+            instance.average_rating = Decimal(instance.average_rating).quantize(Decimal('0.0'), rounding=ROUND_HALF_UP)
+        return instance
+
+    async def get_memory_variations(self, instance: Model) -> Model:
+        instance.memory_variations = (await self.manager.execute(
+            self.select_visible(self.Model.memory)
+            .where(self.Model.model == instance.model).distinct())).scalars().all()
+        return instance
+
+    async def get_color_variations(self, instance: Model) -> List:
+        color_variations = (
+            await self.manager.execute(
+                select(Product.color_main, Product.color_hex)
+                .distinct()
+                .where(Product.type == instance.type)
+            )
+        ).all()
+        instance.color_variations = [{'color': color, 'hex': hex_value} for color, hex_value in color_variations]
         return instance
 
     async def get_reviews(self, instance: Model) -> Model:
-        instance.reviews = (await self.manager.execute(
+        reviews = (await self.manager.execute(
             select(
-                Review.id, Review.rating, Review.text,
+                Review.id, Review.rating, Review.text, Review.date_created,
                 func.coalesce(
                     func.concat(User.first_name, ' ', User.last_name), None
-                ).label('user')).outerjoin(User, Review.id_user == User.id)
-            .filter(Review.id_product == instance.id))).all()
+                ).label('user'), User.photo_url
+            )
+            .outerjoin(User, Review.id_user == User.id)
+            .filter(Review.id_product == instance.id)
+        )).mappings().all()
+
+        modified_reviews = []
+        for review in reviews:
+            review_dict = dict(review)
+            review_dict['photo_url'] = settings_app.BASE_URL + review_dict['photo_url']
+            modified_reviews.append(review_dict)
+        instance.reviews = modified_reviews
+        return instance
+
+    @staticmethod
+    def get_updated_photo_url(instance: Model) -> Model:
+        for photo in instance.photos:
+            if not photo.url.startswith('http'):
+                photo.url = settings_app.BASE_URL + photo.url
+        return instance
+
+    async def check_product_in_cart(self, instance: Model) -> Model:
+        is_in_cart = (
+                await self.manager.execute(
+                    select([exists().where(
+                        (Order.id_user == self.id_user) &
+                        (Order.id == OrderItem.id_order) &
+                        (OrderItem.id_product == instance.id)
+                    )])
+                )
+            ).scalar()
+        if is_in_cart:
+            instance.is_in_cart = is_in_cart
         return instance
 
     @staticmethod
@@ -70,11 +131,19 @@ class ProductService(ServiceBase):
             .options(selectinload(Product.photos))
             .where(self.Model.id == id_instance))).scalars().first()
 
+        if not instance:
+            raise HTTPException(status_code=404, detail="Такого товара не существует")
+
         instance.is_favourite = False
         await self.get_rating_and_reviews_count(instance)
+        await self.get_reviews(instance)
         if self.id_user:
             await self.check_favourite(instance, favourite_service)
-        return await self.get_reviews(instance)
+        if hasattr(instance, 'memory'):
+            await self.get_memory_variations(instance)
+        await self.get_color_variations(instance)
+        self.get_updated_photo_url(instance)
+        return instance
 
     async def get_instance(self, id_instance: int) -> Model:
         return (await self.manager.execute(
@@ -98,13 +167,14 @@ class ProductService(ServiceBase):
         query: Optional[Select] = None,
     ):
         result = await self.list(filters=filters, orderings=orderings, pagination=pagination, query=query)
-        for product in result['items']:
-            await self.get_rating_and_reviews_count(product)
-            product.is_favourite = False
-
         if self.id_user:
             for product in result['items']:
                 await self.check_favourite(product, favourite_service)
+                await self.check_product_in_cart(product)
+        for product in result['items']:
+            await self.get_rating_and_reviews_count(product)
+            self.get_updated_photo_url(product)
+            product.is_favourite = False
         return result
 
     async def get_fragment(self, query: Select, limit: Optional[int], offset: int) -> Tuple[List, int]:
